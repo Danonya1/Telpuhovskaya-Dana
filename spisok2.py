2bc65fee5611758de176006b42fd08666350bd5e
Danonya1 <112933917+Danonya1@users.noreply.github.com>
2023-06-20 01:59:44 +0300

    Add files via upload

---
+++ spisok2.py
@@ -0,0 +1,14 @@
+import random
+mylist = []
+for i in (range(50,100)):
+    mylist.append(random.randint(0,9999))
+import random
+mylist2 = []
+for i in (range(50,100)):
+    mylist2.append(random.randint(0,9999))
+print('first:',mylist, 'second:', mylist2,)
+mylist3 = [] 
+for i in mylist:  
+    if i in mylist2:  
+        mylist3.append(i) 
+print(f"[{mylist3}]")
